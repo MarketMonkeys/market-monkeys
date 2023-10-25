55d6e1203294d790232d33ecfdfae6f5e319f0a8
vzhytnyk <vzhytnyk@market-monkeys.com>
2023-10-25 20:47:02 +0300

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,38 @@
-// import Image from "next/image";
+import Image from "next/image";
+import styles from "./page.module.css";
+import BlueBlock from "@/pages/About/BlueBlock/BlueBlock";
 
 export default function About() {
   return (
-    <main>
-      {/* <Image
-        src="/images/about-bg.png"
-        alt="About bg"
-        // width={1440}
-        // height={770}
-        style={{ objectFit: "contain" }}
-        fill={true}
-      /> */}
-      <h1>About</h1>
-    </main>
+    <section>
+      <div
+        style={{
+          width: "100%",
+          height: "770px",
+          position: "absolute",
+          top: "0",
+          left: "0",
+        }}
+      >
+        <Image
+          src="/images/about-bg-1200.png"
+          alt="About bg"
+          className={styles.imgSmall}
+          priority={true}
+          fill={true}
+        />
+        <Image
+          src="/images/about-bg-1920.png"
+          alt="About bg"
+          className={styles.imgLarge}
+          priority={true}
+          fill={true}
+        />
+      </div>
+      <div className={styles.contentImgWrapper}>
+        <h1>About</h1>
+      </div>
+      <BlueBlock />
+    </section>
   );
 }
