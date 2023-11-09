68c2b9d4d475399e535b7aa57c1cbc30c1f4bb03
MarketMonkeys <admin@market-monkeys.com>
2023-11-09 11:47:17 +0200

    Changed Carousel in Our team section - changed photos and names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import AdvatagesSection from "@/pages/Home/AdvantagesSection/AdvantagesSection";
-import DigitalMarketingSection from "@/pages/Home/DigitalMarketingSection/DigitalMarketingSection";
-import ProjectPortfolioSection from "@/pages/Home/ProjectPortfolioSection/ProjectPortfolioSection";
-import ServiceSpectrumSection from "@/pages/Home/ServiceSpectrumSection/ServiceSpectrumSection";
-import TeamSection from "@/pages/Home/TeamSection/TeamSection";
+import AdvatagesSection from '@/pages/Home/AdvantagesSection/AdvantagesSection';
+import DigitalMarketingSection from '@/pages/Home/DigitalMarketingSection/DigitalMarketingSection';
+import ProjectPortfolioSection from '@/pages/Home/ProjectPortfolioSection/ProjectPortfolioSection';
+import ServiceSpectrumSection from '@/pages/Home/ServiceSpectrumSection/ServiceSpectrumSection';
+import TeamSection from '@/pages/Home/TeamSection/TeamSection';
 
 export default function Home() {
   return (
@@ -10,13 +10,13 @@ export default function Home() {
       <DigitalMarketingSection />
       <div
         style={{
-          background: "var(--blue)",
-          borderRadius: "50px",
-          padding: "120px",
-          position: "relative",
+          background: 'var(--blue)',
+          borderRadius: '50px',
+          padding: '120px',
+          position: 'relative',
         }}
       >
-        <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
+        <div style={{ maxWidth: '1200px', margin: '0 auto' }}>
           <ServiceSpectrumSection />
           <AdvatagesSection />
         </div>
