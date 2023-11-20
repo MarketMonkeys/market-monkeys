ca2778d1cc30efd7c5b5249b3d8d99f0afbed0f2
MarketMonkeys <admin@market-monkeys.com>
2023-11-20 16:08:56 +0200

    Implemented contacts page

---
+++ src/common/mocks/contacts.tsx
@@ -0,0 +1,31 @@
+export const CONTACT_INFO = {
+  email: 'admin@market-monkeys.com',
+  phones: [
+    {
+      country: 'PL',
+      value: '+48 511 525 892',
+    },
+    {
+      country: 'UA',
+      value: '+38 099 986 72 20',
+    },
+  ],
+  socialMedia: [
+    {
+      name: 'Instagram',
+      link: 'https://www.instagram.com/',
+    },
+    {
+      name: 'Telegram',
+      link: 'https://web.telegram.org/a/',
+    },
+    {
+      name: 'Facebook',
+      link: 'https://uk-ua.facebook.com/',
+    },
+    {
+      name: 'LinkedIn',
+      link: 'https://ua.linkedin.com/',
+    },
+  ],
+};
